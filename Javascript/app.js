12bd66634d3bc567ac8dc91cd90cbd88c04e8ef5
Joel Margosian <margosij@gmail.com>
2019-09-19 06:33:52 -0400

    have gifs working now for added buttons, might be a better way to work it though

--- Javascript/app.js
+++ Javascript/app.js
@@ -12,6 +12,54 @@ $("#animal-addition").on("click", function(event){
         document.body.appendChild(btn);
         $(btn).attr("value", newAnimal);
 
+
+
+
+        
+
+        $("button").on("click", function() {
+            event.preventDefault();
+        
+            //this needs fixing to pull correct gifs
+            var search = $(this).val();
+        
+        
+            //making an api key variable just to have it saved on here. don't plan on actually using it in code anywhere
+            // var APIKey = "wu03X6rnZ6zfygqbLEfVz3q5hJDvGRCj"
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=wu03X6rnZ6zfygqbLEfVz3q5hJDvGRCj&limit=10";
+        
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            })
+        
+            .then(function(response) {
+                var results = response.data;
+        
+                for (var i = 0; i < results.length; i++) {
+        
+                  if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var animalImage = $("<img>");
+                    animalImage.attr("src", results[i].images.fixed_height.url);
+                    gifDiv.append(p);
+                    gifDiv.append(animalImage);
+        
+                    $("#Gifs").prepend(gifDiv);
+                  }
+                }
+                
+              });
+        // $("#Gifs").html(buttonList)
+        })
+
+
+
+
+
+
     console.log(topics)
     // createButtons();
 })
