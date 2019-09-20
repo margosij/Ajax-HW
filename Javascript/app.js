210a8de6d7e86047ab0182ad5004126674f5ae8c
Joel Margosian <margosij@gmail.com>
2019-09-20 19:24:40 -0400

    functionality done

--- Javascript/app.js
+++ Javascript/app.js
@@ -36,14 +36,13 @@ $("#animal-addition").on("click", function(event){
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
                     var animalImage = $("<img>");
-                    // animalImage.attr("src", results[i].images.fixed_height.url);
                     animalImage.attr("src", results[i].images.fixed_height_still.url);
                     animalImage.attr("data-state", "still");
                     animalImage.attr("data-still", results[i].images.fixed_height_still.url);
                     animalImage.attr("data-animate", results[i].images.fixed_height.url);
                     animalImage.addClass("animalGifs");
-                    gifDiv.prepend(p);
-                    gifDiv.prepend(animalImage);
+                    gifDiv.append(p);
+                    gifDiv.append(animalImage);
 
                     $("#Gifs").prepend(gifDiv);
                   }
@@ -52,7 +51,22 @@ $("#animal-addition").on("click", function(event){
               });
 
         });
-    });
+
+        $(document).on("click", ".animalGifs", function(){
+
+            var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            }
+    
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+});
+});
 
 function createButtons(){
     for (var i=0; i<topics.length; i++){
@@ -69,6 +83,7 @@ $("button").on("click", function() {
     event.preventDefault();
 
     var search = $(this).val();
+
 
     // var APIKey = "wu03X6rnZ6zfygqbLEfVz3q5hJDvGRCj"
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=wu03X6rnZ6zfygqbLEfVz3q5hJDvGRCj&limit=10";
@@ -93,8 +108,8 @@ $("button").on("click", function() {
             animalImage.attr("data-still", results[i].images.fixed_height_still.url);
             animalImage.attr("data-animate", results[i].images.fixed_height.url);
             animalImage.addClass("animalGifs");
-            gifDiv.prepend(p);
-            gifDiv.prepend(animalImage);
+            gifDiv.append(p);
+            gifDiv.append(animalImage);
 
             $("#Gifs").prepend(gifDiv);
           }
